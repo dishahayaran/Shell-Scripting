1a7c89376f43ecf6beb3bb382b1a71bf331db2e2
Ubuntu <ubuntu@ip-172-31-23-1.ec2.internal>
2024-01-26 18:32:20 +0000

    More about variables

---
+++ variables01.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Author: Disha Hayaran
+
+name="Disha Hayaran"
+score=100
+role="Devops Engineer"
+
+echo "Hello!, My name is $name and I'm working as a $role. My score is $score"
+
+role="Senior Engineer"
+
+echo "Now role is : $role"
